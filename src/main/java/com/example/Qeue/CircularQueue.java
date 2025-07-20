66770a90d967098937c85f54bfd17741a68d9eac
Mohamed-Irfan-git <irfanmt29@gmail.com>
2025-07-20 17:48:27 +0530

    circularqueue

---
+++ src/main/java/com/example/Qeue/CircularQueue.java
@@ -0,0 +1,58 @@
+package com.example.Qeue;
+
+
+public class CircularQueue {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    protected int front= 0;
+    protected int end = 0;
+
+    //When we not give any size it set it ti the default one
+    public CircularQueue() {
+        this(DEFAULT_SIZE);
+    }
+    public CircularQueue(int size) {
+        this.data=new int[size];
+    }
+    public void insert(int val) throws QueueException{
+        if(isFull()){
+            throw new QueueException("Queue is full");
+        }
+
+        data[end++]=val;
+        end= end% data.length;
+        size++;
+
+    }
+    public int remove(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
+        }
+        int temp=data[front++];
+        front= front% data.length;
+        size--;
+        return temp;
+    }
+
+    private boolean isEmpty() {
+        return front == 0;
+    }
+
+
+    private boolean isFull() {
+        return size == data.length;
+    }
+    public void display(){
+        int n = front;
+        do{
+            System.out.print(data[n]+" ");
+            n++;
+            n = n%data.length;
+
+        }while(n!=end);
+        System.out.println("END");
+    }
+
+}
--- src/main/java/com/example/Qeue/Main.java
+++ src/main/java/com/example/Qeue/Main.java
@@ -2,15 +2,24 @@ package com.example.Qeue;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        CustomQueue customQeue = new DynamicQueue(5);
-        customQeue.insert(15);
-        customQeue.insert(10);
-        customQeue.insert(20);
-        customQeue.insert(30);
-        System.out.println(customQeue.remove());
-        customQeue.insert(42);
-        customQeue.insert(28);
-        System.out.println(customQeue.remove());
-        customQeue.display();
+//        CustomQueue customQeue = new DynamicQueue(5);
+//        customQeue.insert(15);
+//        customQeue.insert(10);
+//        customQeue.insert(20);
+//        customQeue.insert(30);
+//        System.out.println(customQeue.remove());
+//        customQeue.insert(42);
+//        customQeue.insert(28);
+//        System.out.println(customQeue.remove());
+//        customQeue.display();
+
+        CircularQueue circularQueue = new CircularQueue(4);
+        circularQueue.insert(1);
+        circularQueue.insert(2);
+        circularQueue.insert(3);
+        System.out.println(circularQueue.remove());
+        circularQueue.insert(4);
+        circularQueue.insert(5);
+        circularQueue.display();
     }
 }
