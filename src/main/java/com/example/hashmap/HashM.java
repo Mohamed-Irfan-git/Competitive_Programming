eacbf53e30aa248e25cb01449b41fb6f90d3cabf
Mohamed-Irfan-git <irfanmt29@gmail.com>
2025-07-24 16:55:51 +0530

    hashmap

---
+++ src/main/java/com/example/hashmap/HashM.java
@@ -0,0 +1,90 @@
+package com.example.hashmap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashM <T extends Comparable<T>> {
+    private ArrayList <T> list;
+
+    public HashM() {
+        list = new ArrayList<>();
+    }
+
+    private void swap(int first,int second){
+        T tmp = list.get(first);
+        list.set(first, list.get(second));
+        list.set(second,tmp);
+
+    }
+    private int parent(int  index){
+        return  (index-1)/2;
+    }
+    private int left(int  index){
+        return 2*index+1;
+    }
+    private int right(int  index){
+        return 2*index+2;
+    }
+    public void insert(T value){
+        list.add(value);
+        upheap(list.size()-1);
+
+    }
+    private void upheap(int index){
+        if(index==0){
+            return;
+        }
+        int p = parent(index);
+        if(list.get(index).compareTo(list.get(p)) < 0){
+            swap(index,p);
+            upheap(p);
+        }
+    }
+
+    public T remove() throws Exception{
+       if(list.isEmpty()){
+           throw new Exception("List is empty");
+       }
+       T temp = list.get(0);
+       T num = list.remove(list.size()-1);
+       if(!list.isEmpty()){
+           list.set(0,num);
+           downHeap(0);
+       }
+       return  temp;
+    }
+
+    private void downHeap(int index){
+
+        int min = index;
+        int left  = left(index);
+        int right = right(index);
+
+        if(left<list.size()-1 && list.get(left).compareTo(list.get(min)) < 0){
+            min = left;
+        }
+        if(right<list.size()-1 && list.get(right).compareTo(list.get(min)) > 0){
+            min = right;
+        }
+        if(min!=index){
+            swap(min,index);
+            downHeap(min);
+        }
+    }
+
+    public ArrayList<T> heapSort() throws Exception {
+        ArrayList<T> list1 = new ArrayList<>();
+        while(!list.isEmpty()){
+            list1.add(this.remove());
+
+        }
+        return list1;
+    }
+
+    public ArrayList<T> displayAll(){
+        ArrayList<T> lis = new ArrayList<>(list);
+       return lis;
+    }
+
+
+}
